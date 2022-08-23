f3730f5f545f54ac83c0ce4e78ff52eb9b864347
Igor Smorodsky <ingerard@gmail.com>
2022-08-23 15:47:27 +0300

    finished homework

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,18 +1,65 @@
-import {load, save} from "./storage";
+import { load, save, remove } from "./storage";
 import _ from "lodash";
 
-const email = document.querySelector('input[name="email"]');
-const message = document.querySelector('textarea[name="message"]');
+window.addEventListener("load", (event) => {
+    // Name of field in the storage
+    const storageName = "feedback-form-state";
 
-console.log(email);
-console.log(message);
+    // Refs to elements
+    const elements = {
+        email: document.querySelector('input[name="email"]'),
+        message: document.querySelector('textarea[name="message"]'),
+        button: document.querySelector("button"),
+    };
 
-email.addEventListener('input', func);
-message.addEventListener('input', func);
+    // Load saved data
+    function loadData() {
+        try {
+            const { email, message } = load(storageName);
+            elements.email.value = email;
+            elements.message.value = message;
+        } catch (error) {
+            console.log("Storage is empty");
+        }
+    }
 
+    // Load
+    loadData();
 
-function func(params) {
+    // Save data on typing in the inputs
+    function onInput() {
+        save(storageName, {
+            email: elements.email.value,
+            message: elements.message.value,
+        });
+    }
 
-    console.log('input');
-    
-}
+    // Clear all saved data from storage and clear fields
+    function clearStorage() {
+        elements.email.value = "";
+        elements.message.value = "";
+        remove(storageName);
+    }
+
+    // Post data
+    function postData(e) {
+        e.preventDefault();
+        const email = elements.email.value;
+        const message = elements.message.value;
+
+        if (email || message) {
+            console.log(`Email: ${email}, Message: ${message}`);
+            clearStorage();
+        }
+    }
+
+    // Throttle on typing
+    const throttled = _.throttle(() => onInput(), 500);
+
+    // Events
+    elements.email.addEventListener("input", throttled);
+    elements.message.addEventListener("input", throttled);
+    elements.button.addEventListener("click", (e) => {
+        postData(e);
+    });
+});
--- src/js/storage.js
+++ src/js/storage.js
@@ -2,16 +2,23 @@ const save = (key, value) => {
     try {
         const serializedState = JSON.stringify(value);
         localStorage.setItem(key, serializedState);
-        console.log('save');
     } catch (error) {
         console.error("Set state error: ", error.message);
     }
 };
 
 const load = (key) => {
-    try {
+    try {        
         const serializedState = localStorage.getItem(key);
         return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (error) {
+        console.error("Get state error: ", error.message);
+    }
+};
+
+const remove = (key) => {
+    try {
+        localStorage.removeItem(key);
     } catch (error) {
         console.error("Get state error: ", error.message);
     }
@@ -20,4 +27,5 @@ const load = (key) => {
 export {
     save,
     load,
+    remove,
 };
